c52ee5b83be8c8765964b283b68e28196eb21aa8
Are Husby <ahusby@gmail.com>
2015-05-14 14:03:45 +0200

    Mer higher order functions

---
+++ 09chapter/FileMatcher.scala
@@ -0,0 +1,26 @@
+import java.nio.file.Paths
+
+def filenamesEnding(query: String) = {
+  namesMatching(_.endsWith(query))
+}
+
+def filenamesStarting(query: String) = {
+  namesMatching(_.startsWith(query))
+}
+
+def filenamesContaining(query: String) = {
+  filnames().filter(_.contains(query))
+}
+
+
+private def filnames() = Paths.get("/home/arehus").toFile.list
+
+private def namesMatching(matcher: String => Boolean) = {
+  for (name <- filnames()
+       if matcher(name)
+  ) yield name
+}
+
+println("start: " + filenamesStarting("D").mkString(", "))
+println("end: " + filenamesEnding("s").mkString(", "))
+println("contains: " + filenamesEnding("a").mkString(", "))
